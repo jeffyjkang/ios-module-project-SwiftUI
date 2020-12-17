a47a02a1f8135cbfedd83c66b53360e28eb2b200
jeff kang <jeffkang@Jeffs-MBP.attlocal.net>
2020-12-16 20:05:24 -0600

    part 1 complete

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeff Kang on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
